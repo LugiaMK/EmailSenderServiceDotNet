7ea6bee8a522fab6c71e2a35e6ab361b5775dffc
LugiaMK <134388372+LugiaMK@users.noreply.github.com>
2023-09-24 16:31:43 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using EmailService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EmailService.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmailSender _emailSender;
+
+        public HomeController(IEmailSender emailSender)
+        {
+            this._emailSender = emailSender;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var receiver = "myemail@gmail.com";
+            var subject = "Subject";
+            var message = "Hello World";
+
+            await _emailSender.SendEmailAsync(receiver, subject, message);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
